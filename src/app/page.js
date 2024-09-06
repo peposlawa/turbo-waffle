a7e48f99159e3e64fdaf55ffc67a300a07a43992
peposlawa <peposlawa@gmail.com>
2024-09-06 11:01:31 +0100

    braking stuff again for testing - just img test

--- src/app/page.js
+++ src/app/page.js
@@ -2,9 +2,6 @@ import Image from 'next/image';
 import styles from './page.module.css';
 
 export default function Home() {
-  
-  console.log(    'Hello, World!'    );
-
   return (
     <main className={styles.main}>
       <div className={styles.description}>
